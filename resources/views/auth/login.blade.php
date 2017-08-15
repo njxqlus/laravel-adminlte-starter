d588e89120b73a10dfa0eb04a5aeb74bda9ac91c
Unknown <njxqlus@gmail.com>
2017-08-15 17:35:51 +0300

    Login template

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,102 @@
+@extends('admin-lte.layouts.login')
+
+@section('page-title', 'Login')
+
+@section('css')
+
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{asset('admin-lte/plugins/iCheck/square/blue.css')}}">
+
+@endsection
+
+@section('content')
+
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="#">{{ config('app.name', 'Laravel') }}</a>
+        </div>
+        <!-- /.login-logo -->
+
+        <div class="login-box-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+
+            <form action="{{ route('login') }}" method="POST">
+
+                {{ csrf_field() }}
+
+                <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <input type="email" class="form-control" placeholder="Email" value="{{ old('email') }}" name="email"
+                           required
+                           autofocus>
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <input type="password" class="form-control" placeholder="Password" name="password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="remember"
+                                       class="icheck" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
+            <div class="social-auth-links text-center">
+                <p>- OR -</p>
+                <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>
+                    Sign in using Facebook
+                </a>
+                <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i>
+                    Sign in using Google+
+                </a>
+            </div>
+            <!-- /.social-auth-links -->
+
+            <a href="{{ route('password.request') }}">I forgot my password</a><br>
+            <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+
+        </div>
+        <!-- /.login-box-body -->
+    </div>
+    <!-- /.login-box -->
+
+@endsection
+
+@section('scripts')
+
+    <!-- iCheck -->
+    <script src="{{asset('admin-lte/plugins/iCheck/icheck.min.js')}}"></script>
+
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+
+@endsection
