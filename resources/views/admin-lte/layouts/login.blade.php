78182b8a0a14df1638eb989b8183469a4757067d
Unknown <njxqlus@gmail.com>
2017-08-15 17:55:13 +0300

    Registration template

---
+++ resources/views/admin-lte/layouts/login.blade.php
@@ -0,0 +1,9 @@
+@include('admin-lte.parts.html-header')
+
+<body class="hold-transition login-page">
+
+@yield('content')
+
+</body>
+
+@include('admin-lte.parts.html-footer')
---
+++ resources/views/admin-lte/layouts/register.blade.php
@@ -0,0 +1,9 @@
+@include('admin-lte.parts.html-header')
+
+<body class="hold-transition register-page">
+
+@yield('content')
+
+</body>
+
+@include('admin-lte.parts.html-footer')
