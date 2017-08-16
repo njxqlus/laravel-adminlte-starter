a650790cfc465f31bb15854abbfd1445dbc63dbd
Unknown <njxqlus@gmail.com>
2017-08-16 14:20:05 +0300

    Edit company name and current year in footer

--- resources/views/admin-lte/parts/footer.blade.php
+++ resources/views/admin-lte/parts/footer.blade.php
@@ -5,5 +5,5 @@
         Anything you want
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{ \Carbon\Carbon::now()->year }} <a href="#">{{ config('app.name', 'Laravel') }}</a>.</strong> All rights reserved.
 </footer>
